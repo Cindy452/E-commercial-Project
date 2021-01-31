c1521481cbf9bf42d359080491d045c7a69aea89
Your Name <you@example.com>
2021-01-30 19:16:26 -0600

    added cart dropdown

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,8 +4,10 @@ import {auth} from '../../firebase/firebase.util';
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import {connect} from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon';
+import CartDropdown from '../cart-dropdown/cart-dropdown';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
        <Link className='logo-container' to='/'>
         <Logo className='logo' />
@@ -23,14 +25,19 @@ const Header = ({currentUser}) => (
            : 
            <Link className='option' to='/signin'>Sign In</Link>
          }
-    
+         <CartIcon />
        </div>
+       {
+         hidden ? null :  <CartDropdown />
+       }
+      
     </div>
 )
 
 
-const  mapStateToProps = state => ({
-   currentUser: state.user.currentUser
+const  mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+   currentUser,
+   hidden
 })
 
 
